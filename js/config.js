0ab685834c97cbde43129252f424a9a71528c2c7
Lahiru Jayaratne <lahiruj5@gmail.com>
2019-06-27 15:55:58 +0530

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_hJj8xScIY', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7u0mrd41qig4bafpja9ah2i16i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-1' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_F5r7lDyHC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4h745qe6opear3i6i6aq448cfb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
